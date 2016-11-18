c1f3dfeb5871872b9fce98e97c6f5322146e1ee4
Qronnmark <q.ronnmark@sjasd.ca>
2016-11-18 10:54:31 -0600

    Finished code
    10/10 "like Skyrim, with code."-Ign

---
+++ src/FizzBuzz.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author Q.Ronnmark
+ * Quenton Ronnmark
+ * Mr. Hardman
+ * Assignment 4 part 3
+ * Nov. 16th 2016
+ *
+ */
+public class FizzBuzz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 1; i <= 100; i++ ) {
+		
+			if(i % 3 == 0 && i % 5 == 0){
+				System.out.println("FizzBuzz");
+			}
+			else if(i % 3 == 0){
+				System.out.println("Fizz");
+			}
+			else if(i % 5 == 0){
+				System.out.println("Buzz");		
+			}
+			else{
+				
+				System.out.println(i);
+			}
+		}
+	}
+
+}
